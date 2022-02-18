aa7a170039cadf03565d91f658757f9d48430baa
roshray <roshanroy759@gmail.com>
2022-02-18 23:20:38 +0530

    SearchPage Items

--- vsearch/src/useGoogleSearch.js
+++ vsearch/src/useGoogleSearch.js
@@ -9,7 +9,19 @@ const [data, setData] = useState(null)
 
 useEffect(() => {
 
+    const fetchData = async() => {
+        fetch(
+            `https://googleapis.com/customersearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+        )
+        .then(response => response.json())
+        .then(result => {
+            setData(result)
+        })
+    }
+    fetchData()
 }, [term])
+
+    return { data }
 }
 
 export default useGoogleSearch
